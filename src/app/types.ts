39ec7aa532f3c30554f6df35a76d78c254bea534
benCoder01 <benedikt-ricken@gmx.de>
2020-12-22 18:49:03 +0100

    angular material, core service files, types

---
+++ src/app/types.ts
@@ -0,0 +1,42 @@
+export interface Invoice {
+  sender: Company;
+  recipient: Company;
+  invoiceDate: Date;
+  number: string;
+  dateOfService: Date;
+  eventName?: Event;
+  positions: InvoicePosition[];
+  addition: string;
+}
+
+export interface Company {
+  id: number;
+  address: Address;
+  eMail: string;
+  iban: string;
+  swift: string;
+  vatId: string;
+  bankName: string;
+}
+
+export interface InvoicePosition {
+  name: string;
+  value: number;
+  tax: number;
+  currency: string;
+}
+
+export interface Address {
+  company: string;
+  careOf: string;
+  street: string;
+  zipcode: string;
+  town: string;
+  country: string;
+  postbox: boolean; // TODO: Test if fields can be used for postbox
+}
+
+export interface Event {
+  name: string;
+  date: Date;
+}
